ea1d663f01c7a124be8f502cb8e2f093c1638f42
abelzer09 <aaronrbelzer@gmail.com>
2022-03-31 18:45:49 -0500

    set time block rows to work for time of day in relation to css classes. made currentDay id display current day. made save button target each time block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,23 @@
 var saveButtonEl = $('.BtnSave');
-var task = $('.description');
+var timeBlockEl = $(".time-block");
+var today = moment();
+
+
+$('#currentDay').text(today.format('dddd, MMMM do'));
+
 
 saveButtonEl.on("click", function(){
-    alert('hello');
+    var text = $(this).parent().children('textarea').val();
+    
+    
 })
 
+for (let i = 0; i < timeBlockEl.length; i++) {
+    if($(timeBlockEl[i]).attr("data-time") < moment().hour()){
+        $(timeBlockEl[i]).addClass("past")
+    }else if($(timeBlockEl[i]).attr("data-time") > moment().hour()){
+        $(timeBlockEl[i]).addClass("future")
+    } else{
+        $(timeBlockEl[i]).addClass("present")
+    }
+}
