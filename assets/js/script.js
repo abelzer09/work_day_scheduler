0710a912ee0e555b2db93ac3a1daac9279c6b985
abelzer09 <aaronrbelzer@gmail.com>
2022-03-31 20:57:45 -0500

    added save icon from frontawsome to index.html file save buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,18 @@ var saveButtonEl = $('.BtnSave');
 var timeBlockEl = $(".time-block");
 var today = moment();
 
-
+// sets text of id to current day 
 $('#currentDay').text(today.format('dddd, MMMM do'));
 
 
 saveButtonEl.on("click", function(){
+    // alert('hello');
+    console.log($(this).parent().children('textarea').val());
     var text = $(this).parent().children('textarea').val();
-    
     
 })
 
+// takes the timeblock and puts them in an array and applys css base on time of day in correlation to time assigned to each time block
 for (let i = 0; i < timeBlockEl.length; i++) {
     if($(timeBlockEl[i]).attr("data-time") < moment().hour()){
         $(timeBlockEl[i]).addClass("past")
