73a3bb7e2ef77f1310914bd9b4890d069c63a252
abelzer09 <aaronrbelzer@gmail.com>
2022-03-31 22:17:08 -0500

    made variable in saveButtonEl.on for data-time block. set hour and text to be saved to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,12 @@ $('#currentDay').text(today.format('dddd, MMMM do'));
 
 
 saveButtonEl.on("click", function(){
-    // alert('hello');
-    console.log($(this).parent().children('textarea').val());
     var text = $(this).parent().children('textarea').val();
+    var hour = $(this).parent().data("time");
+    localStorage.setItem(hour,text);
     
 })
+
 
 // takes the timeblock and puts them in an array and applys css base on time of day in correlation to time assigned to each time block
 for (let i = 0; i < timeBlockEl.length; i++) {
@@ -22,4 +23,5 @@ for (let i = 0; i < timeBlockEl.length; i++) {
     } else{
         $(timeBlockEl[i]).addClass("present")
     }
+    // console.log(timeBlockEl)
 }
